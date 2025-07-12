dd95bda8bb3f86d676c7323012fb9b6ecc8f0a4f
Jaelynn-Felix15 <jjorgfelix@gmail.com>
2025-07-12 15:52:29 +0200

    inital commit

---
+++ M7/Classwork Activities/L1-A4.py
@@ -0,0 +1,12 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Number 
+print(4)
+
+#Next Line after print
+print("Hello Python \nThis sentence is coming in new line")
+
+#Print all keywords present in Python print("Keyword in Python are....")
+import keyword
+print(keyword.kwlist)
