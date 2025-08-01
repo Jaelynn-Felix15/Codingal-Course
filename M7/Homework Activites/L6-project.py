83646ae8d41c52088715135aab14d08303963322
Jaelynn-Felix15 <jjorgfelix@gmail.com>
2025-08-01 15:38:10 +0200

    inital commit

---
+++ M7/Homework Activites/L6-project.py
@@ -0,0 +1,23 @@
+import turtle
+
+sc = turtle.Screen()
+# creating canvas 
+sc.bgcolor("pink")
+
+sc.setup(500, 500)
+
+turtle.title("Welcome to Turtle Window")
+
+# turtle object creation 
+board = turtle.Turtle()
+
+board.fillcolor("blue")
+board.begin_fill()
+
+n = 8
+# creating a square
+for i in range(n):
+    board.forward(100)
+    board.left(360/n)
+
+turtle.done()
