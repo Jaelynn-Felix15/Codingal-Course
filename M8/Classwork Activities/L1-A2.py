29b57b6611eed4090c05184a9fec6055ba70683d
Jaelynn-Felix15 <jjorgfelix@gmail.com>
2025-08-09 15:10:53 +0200

    inital commit

---
+++ M8/Classwork Activities/L1-A2.py
@@ -0,0 +1,2 @@
+ # empty dictionary 
+my_dict = {}
---
+++ M8/Classwork Activities/L3-A1.py
@@ -0,0 +1,7 @@
+#OOPS = object oriented programming 
+class student:
+    grade = 9
+    print("Hi I am a stduent of grade ",grade)
+
+ #object of the class 
+obj = student()
