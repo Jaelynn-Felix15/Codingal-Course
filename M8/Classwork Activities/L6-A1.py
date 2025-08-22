e71a0d115eccf373267a312cb06741f457c99c0f
Jaelynn-Felix15 <jjorgfelix@gmail.com>
2025-08-22 15:15:31 +0200

    inital commit

---
+++ M8/Classwork Activities/L6-A1.py
@@ -0,0 +1,31 @@
+#Polymorphism is an object-oriented programming concept that allows objects of different classes to be treated through a common interface, while each object can behave differently based on its class.
+class Cat:
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+        
+    def make_sound(self):
+        print("Meow")
+
+    
+class Dog:
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("Bark")
+
+
+cat1 = Cat("Dodo", 2.5)
+dog1 = Dog("Tyson", 8)
+
+for animal in (cat1, dog1):
+    animal.make_sound()
+    animal.info()
